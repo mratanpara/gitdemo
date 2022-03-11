bc2c1b29d69277f1a5501f439a755d62b90ec346
mratanpara <mohit.ratanpara@solutionanalysts.in>
2022-03-11 12:03:54 +0530

    testing

--- lib/home.dart
+++ lib/home.dart
@@ -1,17 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+const duration = Duration(milliseconds: 500);
+
+class OnBoarding extends StatefulWidget {
+  static const String id = 'onBoarding_screen';
+  const OnBoarding({Key? key}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
+  _OnBoardingState createState() => _OnBoardingState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _OnBoardingState extends State<OnBoarding> {
+  final pages = <Widget>[
+    Container(
+      color: Colors.redAccent,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Align(
+                child: FlutterLogo(size: 150),
+                alignment: Alignment.center,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                  'We’ve Got Your Style',
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                  'Your home is a reflection of you. Nail your aesthetic with products our stylists love and recommend',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+    Container(
+      color: Colors.greenAccent,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Align(
+                child: FlutterLogo(size: 150),
+                alignment: Alignment.center,
+              ),
+              Text(
+                'MAKE YOUR',
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                  'HOME BEAUTIFUL',
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                  'The best simple place wheasdsadre you discover most wonderful furniture\'s and make your home beautiful',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      chid:Text("Hello Mohit!");
+    return Scaffold(
+      body: LiquidSwipe(
+        pages: pages,
+        waveType: WaveType.liquidReveal,
+        fullTransitionValue: 880,
+      ),
     );
   }
 }
